5f35da304cba9534413ea8452f88f69c87279ba8
Your Name <dagmara.ohde@gmail.com>
2019-06-27 17:37:17 +0000

    Modul 6, zadanie 2

--- kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import java.sql.SQLOutput;
+
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(5.0,10.0);
+
+        double resultAdd= calculator.addAtoB();
+        double resultSubtract = calculator.subtractAfromB();
+
+        if(resultAdd == 15.0){
+            System.out.println("Method addAtoB works perfectly.");
+        }else{
+            System.out.println("During testing addAtoB something went wrong...");
+        }
+
+        if(resultSubtract == 5.0){
+            System.out.println("Method subtractAfromB works perfectly.");
+        }else{
+            System.out.println("During testing subtractAformB something went wrong...");
+        }
     }
 
 }
