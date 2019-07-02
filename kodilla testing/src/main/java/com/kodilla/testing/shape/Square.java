09d11f5957b234a695b2b5ba45ddb82ad33e2afe
Your Name <dagmara.ohde@gmail.com>
2019-07-02 20:31:33 +0000

    moduł6, zadanie4

--- kodilla testing/src/main/java/com/kodilla/testing/shape/Square.java
+++ kodilla testing/src/main/java/com/kodilla/testing/shape/Square.java
@@ -1,4 +1,30 @@
 package com.kodilla.testing.shape;
 
-public class Square {
+public class Square implements Shape {
+    private String name;
+    private double a;
+
+    public Square(String name, double a){
+        this.name = name;
+        this.a = a;
+    }
+
+    @Override
+    public String getShapeName() {
+        return this.name;
+    }
+
+    @Override
+    public double getField() {
+        double field = Math.pow(a,2);
+        return field;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "name='" + name + '\'' +
+                ", a=" + a +
+                '}';
+    }
 }
--- kodilla testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,4 +1,68 @@
 package com.kodilla.testing.shape;
 
+import org.junit.*;
+
 public class ShapeCollectorTestSuite {
+
+    private static int counter = 0;
+
+    @Before
+    public void beforeTests() {
+        counter++;
+        System.out.println("running test #" + counter + ".");
+    }
+
+    @After
+    public void afterTests() {
+        System.out.println("test #" + counter + " was run.");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Starting tests.");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("All test are done.");
+    }
+
+    @Test
+    public void testAddFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle someCircle = new Circle("circle", 5.5);
+        //When
+        shapeCollector.addFigure(someCircle);
+        //Then
+        Assert.assertEquals(1, shapeCollector.getFiguresSize());
+    }
+
+    @Test
+    public void testGetFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle someCircle = new Circle("circle", 5.5);
+        shapeCollector.addFigure(someCircle);
+        //When
+        Shape result = shapeCollector.getFigure(0);
+        //Then
+        Assert.assertEquals(someCircle, result);
+    }
+    @Test
+    public void testRemoveFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle someCircle = new Circle("circle", 5.5);
+        shapeCollector.addFigure(someCircle);
+        shapeCollector.showFigures();
+        //When
+        shapeCollector.removeFigure(0);
+        shapeCollector.showFigures();
+        //Then
+        Assert.assertEquals(0,shapeCollector.getFiguresSize());
+    }
+
+
+
 }
