91f052ad1735f6b46068920e31b3b591c2431b95
Your Name <dagmara.ohde@gmail.com>
2019-07-13 19:33:07 +0000

    Moduł 7 zadanie 1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,33 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        PoemDecorator poemDecorator = (a) -> "***" + a + "***";
+        poemBeautifier.beautify("Kodilla course.", (poemDecorator));
+        PoemDecorator poemDecorator2 = (a) -> a.toUpperCase() + " ;)";
+        poemBeautifier.beautify("Kodilla course", (poemDecorator2));
+        PoemDecorator poemDecorator3 = (a) -> "--->" + a + "<---";
+        poemBeautifier.beautify("Kodilla course.", (poemDecorator3));
+    }
+}
