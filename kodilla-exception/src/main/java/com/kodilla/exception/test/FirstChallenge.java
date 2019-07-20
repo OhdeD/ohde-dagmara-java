ecc5f4425883055e49a6eadcf7dad00bf72043c1
Your Name <dagmara.ohde@gmail.com>
2019-07-20 17:43:11 +0000

    Moduł 8, zadanie2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -5,28 +5,34 @@ import java.util.stream.DoubleStream;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        if (b != 0) {
-            return a / b;
-        } else {
-            try {
-                int c = (int)a;
-                int d=(int)b;
-                return c/d;
-            }catch(ArithmeticException e){
-            System.out.println("Something went wrong: " + e);
-        }finally{
-            System.out.println("You can't divide by \"0\".");
-            return 0;
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
-}    /**
+
+    /**
      * This main can throw an ArithmeticException!!!
      *
      * @param args
      */
     public static void main(String[] args) {
+
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+
+        try {
+
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Something went wrong: " + e);
+
+        } finally {
+
+            System.out.println("Remmember that You can't divide by \"0\";)");
+
+        }
     }
 }
