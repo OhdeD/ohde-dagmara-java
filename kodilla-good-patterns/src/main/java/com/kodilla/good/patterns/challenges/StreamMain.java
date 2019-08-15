3f7b937754a7f4f04f12d5089cea1446083356e8
OhdeD <dagmara.ohde@gmail.com>
2019-08-15 12:10:14 +0000

    modul 13, zadanie 1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMain.java
@@ -8,10 +8,10 @@ public class StreamMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-      String movies = movieStore.getMovies().entrySet().stream()
-              .map(e->e.getValue())
-              .flatMap(e->e.stream())
-              .collect(Collectors.joining("!" ));
+        String movies = movieStore.getMovies().entrySet().stream()
+                .map(e -> e.getValue())
+                .flatMap(e -> e.stream())
+                .collect(Collectors.joining("!"));
         System.out.println(movies);
     }
 }
