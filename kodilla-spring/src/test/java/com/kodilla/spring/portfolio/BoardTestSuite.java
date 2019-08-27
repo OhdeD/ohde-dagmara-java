d518e7cdae328f651c490695cbf8c0f4de540a66
OhdeD <dagmara.ohde@gmail.com>
2019-08-27 16:30:14 +0000

    Moduł 14, zadanie 3

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -16,25 +16,22 @@ public class BoardTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Board board = context.getBean(Board.class);
         //When
-
-
         board.getToDoList().getTasks().add("Do tasks 7");
-
         board.getInProgressList().getTasks().add("In progress task 5");
         board.getDoneList().getTasks().add("Done tasks 1-4");
-      String toDo =  board.getToDoList().getTasks()
+
+        String toDo = board.getToDoList().getTasks()
                 .stream()
                 .collect(Collectors.joining(", ", "", ""));
-       String inProgress = board.getInProgressList().getTasks()
+        String inProgress = board.getInProgressList().getTasks()
                 .stream()
                 .collect(Collectors.joining(", ", "", ""));
         String done = board.getDoneList().getTasks()
                 .stream()
-                .collect(Collectors.joining(", ","",""));
+                .collect(Collectors.joining(", ", "", ""));
         //Then
         Assert.assertEquals("Do tasks 7", toDo);
         Assert.assertEquals("In progress task 5", inProgress);
         Assert.assertEquals("Done tasks 1-4", done);
-
     }
 }
